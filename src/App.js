56ad296be92a25340ae03328e3d69555328fcfed
Peter Kang <pxk7062@rit.edu>
2022-11-18 16:09:21 -0500

    Updated app.js to use navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route} from "react-router-dom";
+import About from "./routes/About";
+import Careers from "./routes/Careers";
+import Home from "./routes/Home";
+import Navbar from './Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/careers" element={<Careers />} />
+      </Routes>
+    </>
   );
 }
+export default App;
