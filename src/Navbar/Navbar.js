10dea80bba8a14a2accd9ceb80e79ecf663b08e8
Peter Kang <pxk7062@rit.edu>
2022-11-23 15:40:28 -0500

    Init Version Navbar

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,11 +1,71 @@
 import { Link } from "react-router-dom"
-const Navbar =()=>{
-      return (
-            <div>
-                  <Link to="./">Home</Link>
-                  <Link to="./Work">Work</Link>
-                  <Link to="./Projects">Projects</Link>
-            </div>
+import React from "react";
+
+const Navbar =()=>
+{
+      return 
+      (
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                  <a className="navbar-brand" href="#">
+                        Navbar
+                  </a>
+                  <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-toggle="collapse"
+                        data-target="#navbarNavDropdown"
+                        aria-controls="navbarNavDropdown"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                  >
+                        <span className="navbar-toggler-icon"></span>
+                  </button>
+                  <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul className="navbar-nav">
+                              <li className="nav-item active">
+                                    <a className="nav-link" href="#">
+                                          Home<span className="sr-only">(current)</span>
+                                    </a>
+                              </li>
+                              <li className="nav-item">
+                                    <a className="nav-link" href="./Work">
+                                          Work
+                                    </a>
+                              </li>
+                              <li className="nav-item">
+                                    <a className="nav-link" href="./Projects">
+                                          Projects
+                                    </a>
+                              </li>
+                              <li className="nav-item dropdown">
+                                    <a
+                                    className="nav-link dropdown-toggle"
+                                    href="#"
+                                    id="navbarDropdownMenuLink"
+                                    data-toggle="dropdown"
+                                    aria-haspopup="true"
+                                    aria-expanded="false"
+                                    >
+                                          Dropdown link
+                                    </a>
+                                    <div
+                                    className="dropdown-menu"
+                                    aria-labelledby="navbarDropdownMenuLink"
+                                    >
+                                          <a className="dropdown-item" href="./Home">
+                                                Home
+                                          </a>
+                                          <a className="dropdown-item" href="./Work">
+                                                Work
+                                          </a>
+                                          <a className="dropdown-item" href="./Projects">
+                                                Projects
+                                          </a>
+                                    </div>
+                              </li>
+                        </ul>
+                  </div>
+            </nav>
       )
 }
 export default Navbar;
