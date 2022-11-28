849da62daf936e93618235a9fda6511cba52a6bf
Peter Kang <pxk7062@rit.edu>
2022-11-28 18:33:43 -0500

    Added Templated work, need to fill in the details later

--- src/routes/Work.js
+++ src/routes/Work.js
@@ -1,8 +1,55 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Work = ()=>
 {
-    return(<div>Work</div>);
+    return(
+    <Container>
+        <Row>
+            <Col>
+            Free lancing
+            </Col>
+        </Row>
+        <Row>
+            <Col>
+            </Col>
+        </Row>
+        <Row>
+            <Col>
+            Mindex
+            </Col>
+        </Row>
+        <Row><Col>Software Engineer</Col></Row>
+        <Row>
+            <Col>
+            </Col>
+        </Row>
+        <Row>
+            <Col>
+            QVI
+            </Col>
+        </Row>
+        <Row><Col>Senior Software Engineer</Col></Row>
+        <Row><Col></Col></Row>
+        <Row><Col>Softare Engineer</Col></Row>
+        <Row><Col></Col></Row>
+        <Row><Col>Software Engineer Intern</Col></Row>
+        <Row><Col></Col></Row>
+        <Row>
+            <Col>
+            SyncSum
+            </Col>
+        </Row>
+        <Row>
+            <Col>
+            Software Engineer
+            </Col>
+        </Row>
+        <Row>
+            <Col></Col>
+        </Row>
+    </Container>
+    );
 } 
 
 export default Work;
