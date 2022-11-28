9591cd6428ef4ac9ac98df7f393152d0136881c2
Peter Kang <pxk7062@rit.edu>
2022-11-28 15:43:58 -0500

    Centered Image

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { Row, Col } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import Image from "react-bootstrap/Image"
 
 const Home = ()=>
 {
     return(
-        <div className="justify-content-center">
+        <Container>
             <Row className="justify-content-center">
-                <Col md={2} sm={2} xs={2}>
-                    <Image  src='./profilePhoto.png'/>
+                <Col md={4} sm={4} xs={4} className="m-auto">
+                    <Image class="center-block" src='./profilePhoto.png'/>
                 </Col>
             </Row>
             <Row className="justify-content-center">
@@ -16,13 +16,13 @@ const Home = ()=>
                     Hi, I'm Peter Kang. A software engineer that works with interesting people on interesting projects.
                 </Col>
             </Row>
-            
+            <br/>
             <Row className="justify-content-center">
                 <Col md={6} sm={8} xs={10}> 
                     I enjoy listening to podcasts, exercising, reading, and learning about new things.
                 </Col>
             </Row>
-        </div>
+        </Container>
     );
 } 
 
