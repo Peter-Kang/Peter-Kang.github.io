6edde514921dd09e2acbbbd0da3e0a01a2584ca4
Peter Kang <pxk7062@rit.edu>
2022-11-29 15:47:55 -0500

    Updated wording on Work page

--- src/routes/Work.js
+++ src/routes/Work.js
@@ -23,6 +23,7 @@ const Work = ()=>
             </Col>
         </Row>
         <Row>
+            <Col md={1} sm={2} xs={2} className="m-auto"><Image src='./mindex.png' width="50" height="50"></Image></Col>
             <Col>
             Mindex
             </Col>
@@ -34,18 +35,22 @@ const Work = ()=>
             </Col>
         </Row>
         <Row>
+            <Col md={1} sm={2} xs={2} className="m-auto"><Image src='./QVI.png' width="50" height="50"></Image></Col>
             <Col>
             QVI
             </Col>
         </Row>
         <Row><Col>Senior Software Engineer</Col></Row>
-        <Row><Col>Added a Pub/Sub implementation in Common Language Infrastructure. Lead software projects like temperature compensation. Mentored other software engineers</Col></Row>
+        <Row><Col>Lead software projects like temperature compensation and event driven actions. Mentored other software engineers.</Col></Row>
         <Row><Col>Softare Engineer</Col></Row>
-        <Row><Col>Worked on Systems Software to control metrology machines. Then worked on both Windows Forms and embeded software that the systems software interfaced with.</Col></Row>
+        <Row><Col>Worked on Systems Software to control metrology machines. Added a Pub/Sub implementation in Common Language Infrastructure. Then worked on both Windows Forms and embeded software that the systems software interfaced with.</Col></Row>
         <Row><Col>Software Engineer Intern</Col></Row>
         <Row><Col>Updated their software project site for tracking Software Change reqests.</Col></Row>
         <Row><Col>Created a .Net MVC5 site done in a MVVM pattern to replace the perfect tracker site.</Col></Row>
-        <Row><Col>SyncSum</Col></Row>
+        <Row>
+            <Col md={1} sm={2} xs={2} className="m-auto"><Image src='./syncsum.png' width="50" height="50"></Image></Col>
+            <Col>SyncSum</Col>
+        </Row>
         <Row><Col>Software Engineer</Col></Row>
         <Row><Col>Summer Project for a Ruby on Rails Applicant tracking system</Col></Row>
     </Container>
