3b148a95bda46edd4b8b4afa8e6bdd4be72d1565
Peter Kang <pxk7062@rit.edu>
2023-01-09 15:28:02 -0500

    Updated the a tags to include hrefs in the project page

--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -11,15 +11,15 @@ const Projects = ()=>
         <Row><Col className="fw-bold"> MTG Draft App </Col></Row>
         <Row><Col>AngularJS Express web app for magic the gathering Drafting</Col></Row>
         <Row><Col>Worked with a friend on putting together an app to draft magic the gathering packs.</Col></Row>
-        <Row><Col> <a>https://github.com/cman131/last-season-received-criticism-for-not-happening-in-a-prison-but-it-turns-out-society-is-the-prison</a> </Col></Row>
+        <Row><Col> <a href="https://github.com/cman131/last-season-received-criticism-for-not-happening-in-a-prison-but-it-turns-out-society-is-the-prison"> https://github.com/cman131/last-season-received-criticism-for-not-happening-in-a-prison-but-it-turns-out-society-is-the-prison</a> </Col></Row>
 
         <Row><Col className="fw-bold"> Hackaton: Hobby Expenditure Calculator </Col></Row>
         <Row><Col> .Net WebApp calculator to help budget how much could be spent a hobby</Col></Row>
-        <Row><Col> <a>https://github.com/cman131/HobbyExpenditureCalculator</a> </Col></Row>
+        <Row><Col> <a href="https://github.com/cman131/HobbyExpenditureCalculator" >https://github.com/cman131/HobbyExpenditureCalculator</a> </Col></Row>
 
         <Row><Col className="fw-bold"> RIT iOS Hackathon: SwiftRekt </Col></Row>
         <Row><Col> Swift app color detection game. Worked with 3 other devs done entirely in swift. Primary spent time on the color detection part and helping the team with things that come up.</Col></Row>
-        <Row><Col> <a>https://github.com/MorganCabral/swiftrekt-ios-app-dev-challenge-2015</a> </Col></Row>
+        <Row><Col> <a href="https://github.com/MorganCabral/swiftrekt-ios-app-dev-challenge-2015">https://github.com/MorganCabral/swiftrekt-ios-app-dev-challenge-2015</a> </Col></Row>
 
     </Container>
     );
