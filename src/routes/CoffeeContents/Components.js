ca67ac804f95411d4f6baaf1568920cb44bd2717
Peter Kang <pxk7062@rit.edu>
2023-02-09 17:38:08 -0500

    Updated spelling

--- src/routes/CoffeeContents/Components.js
+++ src/routes/CoffeeContents/Components.js
@@ -10,7 +10,7 @@ const GaggiuinoComponents = ()=>
         <h2><a id="components"><u> Components</u></a></h2>
         <h4><a id="powersupply">Power Supply</a></h4>
         <body>
-            For the power supply, a AC-DC 5V 2A switching power supply module instead of the recommened 12v/1A power supply and 12v to 5v stepdown. 
+            For the power supply, a AC-DC 5V 2A switching power supply module instead of the recommended 12v/1A power supply and 12v to 5v stepdown. 
             This was mainly because at the time it is cheaper, and was lucky enough to get one that was reliable.
             <br/>
             <figure class="figure" className="text-center">
@@ -22,7 +22,7 @@ const GaggiuinoComponents = ()=>
         <h4><a id="microcontoller">Micro Controller</a></h4>
         <body>
             A Arduino Nano is used to connect the devices and machine. 
-            At the time the parts where ordered the nano was not depercated. Upgrading the to STM32 Blackpill with added features would be the best path forward. It does add a significant amount of <a href="https://gaggiuino.github.io/#/?id=features">features</a>.
+            At the time the parts where ordered the nano was not deprecated. Upgrading the to STM32 Blackpill with added features would be the best path forward. It does add a significant amount of <a href="https://gaggiuino.github.io/#/?id=features">features</a>.
             <br/>
             <figure class="figure" className="text-center">
                 <Image class ="alignmnet-center figure-img img-fluid" src="./CoffeeMachineImages/IMG_2738.png" height="25%" width="25%" alt="On The Top left is the power supply that converts live and neutral to 5v and ground"/>
@@ -58,12 +58,12 @@ const GaggiuinoComponents = ()=>
         <br/>
         <h4><a id="pressure">Pressure</a></h4>
         <body>
-            Giving the machine presure control will also help in better setting the extraction. Presure is measured in bars. 
+            Giving the machine pressure control will also help in better setting the extraction. Pressure is measured in bars. 
             Typically in machines 9 bars is the norm, but my machine has a 6.5 bar spring, so the highest pressure I can apply is 6.5 bars. 
             Slowing down the bar pressures allows the machine to extract the contents from the beans better. 
             A 20g VST basket is also used, where as a traditional basket is 14g. 
             The larger basket allows for double shots of espresso to be pulled from the machine.
-            Fitting the pump sensor into the exisiting pump set up requires a T fitting. 
+            Fitting the pump sensor into the existing pump set up requires a T fitting. 
             <figure class="figure" className="text-center">
                 <Image class ="alignmnet-center figure-img img-fluid" src="./CoffeeMachineImages/IMG_2867.png" height="25%" width="25%" alt="T Fitting"/>
                 <figcaption class="figure-caption">T Fitting, joining 3 orange-red tubes</figcaption>
@@ -73,7 +73,7 @@ const GaggiuinoComponents = ()=>
                 <Image class ="alignmnet-center figure-img img-fluid" src="./CoffeeMachineImages/IMG_2868.png" height="25%" width="25%" alt="Pressure Sensor"/>
                 <figcaption class="figure-caption">Pressure Sensor, the hex shaped fitting attached to the orange-red tube</figcaption>
             </figure>
-            After getting a reading from the pressure sensor, the micro controller then sends signals to a dimmer that adjsuts the pressure of the pump.
+            After getting a reading from the pressure sensor, the micro controller then sends signals to a dimmer that adjusts the pressure of the pump.
             This creates automated pressure control via the micro controller.
             <figure class="figure" className="text-center">
                 <Image class ="alignmnet-center figure-img img-fluid" src="./CoffeeMachineImages/dimmer.jpg" height="25%" width="25%" alt="dimmer"/>
