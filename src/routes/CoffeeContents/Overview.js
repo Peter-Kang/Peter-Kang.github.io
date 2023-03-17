5ce4e76cecc30fefe522f32b5e676a7686e00c98
Peter Kang <pxk7062@rit.edu>
2023-03-17 15:51:08 -0400

    Updated the wording in the overview section

--- src/routes/CoffeeContents/Overview.js
+++ src/routes/CoffeeContents/Overview.js
@@ -8,7 +8,7 @@ const CoffeeOverview = ()=>
         <Container>
             <h2><a id="overview"><u>Overview</u></a></h2>
             <body>
-The upgrade consists of several components such as power, micro controller, display, temperature/boiler, and pressure/pump.
+The upgrade consists of several components such as power, micro controller, display, temperature sensors, boiler control, and pressure sensor, and pump control.
             </body> 
             <div className="text-center">
             <Image  src="https://user-images.githubusercontent.com/42692077/160548957-88c93198-6d81-4081-8db6-552b6f6c5281.png" width="35%" height="35%"></Image></div>
