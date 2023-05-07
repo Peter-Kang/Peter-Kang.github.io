fdba16ecca7c52902e8f389af602261bd48c5b72
Peter Kang <pxk7062@rit.edu>
2023-05-06 23:46:46 -0400

    Updated Projects to include links

--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -8,7 +8,8 @@ const Projects = ()=>
         <Row><Col className="fw-bold"><h3><u>Free Lancing</u></h3></Col></Row>
         <Row><Col> <h5> Convergence Art </h5></Col></Row>
         <Row><Col>Arduino Bluetooth Light controlling art project.</Col></Row>
-        <Row><Col>This art project was done to help a glass making grad student.</Col></Row>
+        <Row><Col>This art project was done to help a Glass grad student.</Col></Row>
+        <Row><Col><a ref="https://github.com/Peter-Kang/FadingLightArudinoProject">https://github.com/Peter-Kang/FadingLightArudinoProject</a></Col></Row>
         <br/>
         <Row><Col><h5> Evenline.co </h5></Col></Row>
         <Row><Col>3D Glass printing start up. Created a UI to control motors using windows forms. C# and C++ project.</Col></Row>
