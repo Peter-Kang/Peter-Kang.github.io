c655c0a1d69e5aef4888a14be5073dc81003d644
Peter-Kang <pkang93@gmail.com>
2023-06-14 20:14:38 -0400

    Updated details

--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -7,7 +7,7 @@ const Projects = ()=>
         <Container>
             <Row><Col className="fw-bold"><h3><u>Full Stack</u></h3></Col></Row>
             <Row><Col> <h6> <b>Reminder App</b> </h6></Col></Row>
-            <Row><Col>A Reminder app built with React, Deno, MongoDB, and Docker. Using Axios for the API calls from React to Deno. Then Oak middleware as a server in Deno</Col></Row>
+            <Row><Col>A Multi-Container Reminder app built with React, Deno, MongoDB, and Docker. Using Axios for the API calls from React to Deno. Then Oak middleware as a server in Deno</Col></Row>
             <Row><Col><a href="https://github.com/Peter-Kang/RemindMeScheduler">https://github.com/Peter-Kang/RemindMeScheduler</a></Col></Row>
             <br/>
             <Row><Col> <h6> <b>Pkang.dev</b> </h6></Col></Row>
