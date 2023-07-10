581bd82369a3a4b00daa5807eac9b539967d9db4
Peter Kang <pxk7062@rit.edu>
2023-07-10 18:40:40 -0400

    Added CryptoCoinCollection

--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -8,16 +8,22 @@ const Projects = ()=>
             <Row><Col className="fw-bold"><h3><u>Full Stack</u></h3></Col></Row>
             <Row><Col> <h6> <b>Reminder App</b> </h6></Col></Row>
             <Row><Col>A Multi-Container Reminder app built with React, Deno, MongoDB, and Docker. Using Axios for the API calls from React to Deno. Then Oak middleware as a server in Deno</Col></Row>
-            <Row><Col><a href="https://github.com/Peter-Kang/RemindMeScheduler">https://github.com/Peter-Kang/RemindMeScheduler</a></Col></Row>
+            <Row><Col><b>Repository:</b><a href="https://github.com/Peter-Kang/RemindMeScheduler">https://github.com/Peter-Kang/RemindMeScheduler</a></Col></Row>
             <br/>
             <Row><Col> <h6> <b>Pkang.dev</b> </h6></Col></Row>
             <Row><Col>Personal site done in react. This a site where I share things I enjoy along with projects I have completed.</Col></Row>
-            <Row><Col><a href="https://github.com/Peter-Kang/Peter-Kang.github.io">https://github.com/Peter-Kang/Peter-Kang.github.io</a></Col></Row>
+            <Row><Col><b>Demo:</b><a href="https://pkang.dev/"> https://pkang.dev/</a></Col></Row>
+            <Row><Col><b>Repository:</b><a href="https://github.com/Peter-Kang/Peter-Kang.github.io">https://github.com/Peter-Kang/Peter-Kang.github.io</a></Col></Row>
+            <br/>
+            <Row><Col> <h6> <b>CryptoCoinCollection</b> </h6></Col></Row>
+            <Row><Col>Displays coin prices. Done with Tram-lite library. </Col></Row>
+            <Row><Col><b>Demo:</b><a href="https://pkang.dev/CryptoCoinCollection/"> https://pkang.dev/CryptoCoinCollection/</a></Col></Row>
+            <Row><Col><b>Repository:</b><a href="https://github.com/Peter-Kang/CryptoCoinCollection">https://github.com/Peter-Kang/CryptoCoinCollection</a></Col></Row>
             <br/>
             <Row><Col className="fw-bold"><h3><u>Back end</u></h3></Col></Row>
             <Row><Col> <h6> <b>DwarfPoolAPIClient</b>  </h6></Col></Row>
             <Row><Col>.Net Core 2.0 Project done to get API calls from DwarfPool, a popular ethereum mining pool at the time.</Col></Row>
-            <Row><Col><a href="https://github.com/Peter-Kang/DwarfPoolAPIClient">https://github.com/Peter-Kang/DwarfPoolAPIClient</a></Col></Row>
+            <Row><Col><b>Repository:</b><a href="https://github.com/Peter-Kang/DwarfPoolAPIClient">https://github.com/Peter-Kang/DwarfPoolAPIClient</a></Col></Row>
         </Container>
     )
 }
