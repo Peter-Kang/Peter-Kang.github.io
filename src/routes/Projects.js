c22f5afd4a71da96fbc7428f24f61837432f45f1
Peter-Kang <pkang93@gmail.com>
2023-10-17 12:56:29 -0400

    Updated Details

--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -21,8 +21,8 @@ const Projects = ()=>
             <Row><Col><b>Demo:</b><a href="https://pkang.dev/WiseWhisperingWontons/"> https://pkang.dev/WiseWhisperingWontons/</a></Col></Row>
             <Row><Col><b>Repository:</b><a href="https://github.com/Peter-Kang/WiseWhisperingWontons">https://github.com/Peter-Kang/WiseWhisperingWontons</a></Col></Row>
             <br/>
-            <Row><Col> <h6> <b>Wise Whispering Wontons</b> </h6></Col></Row>
-            <Row><Col>Fortune cookie app. Done with Tram-lite library. </Col></Row>
+            <Row><Col> <h6> <b>Tip Split</b> </h6></Col></Row>
+            <Row><Col>Tip splitting app For Happy Earth Tea. Done with Tram-lite library. </Col></Row>
             <Row><Col><b>Demo:</b><a href="https://pkang.dev/TipSplit/"> https://pkang.dev/TipSplit/</a></Col></Row>
             <Row><Col><b>Repository:</b><a href="https://github.com/Peter-Kang/TipSplit">https://github.com/Peter-Kang/TipSplit</a></Col></Row>
             <br/>
